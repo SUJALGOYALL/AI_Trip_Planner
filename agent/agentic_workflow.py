5497048286e89a6ab4cd355024643e62e210f3cf
SUJALGOYALL <sujal.230104020@iiitbh.ac.in>
2026-02-09 02:06:35 +0530

    tools and utils updated

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -7,7 +7,7 @@ from langgraph.prebuilt import ToolNode, tools_condition
 from tools.weather_info_tool import WeatherInfoTool
 from tools.place_search_tool import PlaceSearchTool
 from tools.expense_calculator_tool import CalculatorTool
-from tools.currency_converter_tool import CurrencyConverterTool
+from tools.currency_conversion_tool import CurrencyConverterTool
 
 
 class GraphBuilder():
@@ -21,7 +21,7 @@ class GraphBuilder():
         self.currency_converter_tools = CurrencyConverterTool()
 
         self.tools.extend([*self.weather_tools.weather_tool_list,
-                           *self.place_search_tools.place_search_tool_list,
+                        #    *self.place_search_tools.place_search_tool_list,
                            *self.calculator_tools.calculator_tool_list,
                            *self.currency_converter_tools.currency_converter_tool_list])
         self.llm_with_tools = self.llm.bind_tools(tools = self.tools)
